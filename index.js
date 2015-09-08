79d596290c2d03e97e2c61652c8972676f94e185
Benjamin Martin <benjamin.martin@workplacesystems.com>
2015-09-08 15:16:33 +0100

    Added some server info to output. Port now configuratble via ENV[PORT]

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express')
-var app = express()
+
+var express = require('express');
+var os = require('os');
+
+var app = express();
+app.set('port', process.env.PORT || 8081);
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('<h1>Check it!</h1><img src="tug.png" />')
-})
+  res.write('<h1>Look and my lovely tug boat!</h1>')
+  res.write('<img src="tug.png" /> <p> <h2>.. and some sweet cpu info</h2>')
+  cpuinfo = JSON.stringify(os.cpus(),null,'\t')
+  res.end('<pre>'+cpuinfo+'</pre')
+});
 
-app.use(express.static('public', { index: false }));
-
-var server = app.listen(8081, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
